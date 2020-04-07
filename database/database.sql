df72273619857212c7a20815f2e989125340e470
Adrien Barth <ABarth@ptm.net>
2020-04-07 17:49:18 +0200

    docker / api definitions

---
+++ database/database.sql
@@ -0,0 +1,39 @@
+/**
+  Auteurs:  Adrien Barth, Nair Alic
+  Date:     07.04.2020
+  Projet:   Teaching-HEIGVD-IOT-2020-Project-Air-Quality
+
+  Création de la base de données et insertion de valeurs de test.
+ */
+
+DROP DATABASE IF EXISTS iot_air_quality;
+CREATE DATABASE iot_air_quality;
+USE iot_air_quality;
+
+CREATE TABLE devices
+(
+	EUI			varchar(100) not null unique primary key,
+    name		varchar(100) not null,
+    location	varchar(100) null,
+    is_enabled 	tinyint(1) default 1 not null
+);
+
+CREATE TABLE sensors
+(
+    id				int auto_increment primary key,
+    name			varchar(100) not null,
+    fk_device_EUI 	varchar(100) not null,
+    constraint sensors_devices_EUI_fk
+        foreign key (fk_device_EUI) references devices (EUI),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE sensors_data
+(
+    id				int auto_increment primary key,
+	time			datetime not null,
+    value			float not null,
+    fk_sensor_id 	int not null,
+    constraint sensors_data_sensors_id_fk
+        foreign key (fk_sensor_id) references sensors (id)
+);
