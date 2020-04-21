655f7a28f7305fcd0857a90253afd008badb838a
Adrien Barth <ABarth@ptm.net>
2020-04-21 11:48:25 +0200

    Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -41,19 +41,10 @@ CREATE TABLE sensorValues
 INSERT INTO devices VALUES ("0004A30B0024D376", "airquality", "Lausanne", 1);
 INSERT INTO devices VALUES ("0004A30B00215E8C", "environment-2", "Vevey", 1);
 
-INSERT INTO sensors VALUES (0, "Pression", "hPA", "0004A30B00215E8C");
-INSERT INTO sensors VALUES (0, "Température", "Celsius", "0004A30B00215E8C");
-INSERT INTO sensors VALUES (0, "Humidité", "rh", "0004A30B00215E8C");
-INSERT INTO sensors VALUES (0, "Résistance", "Ohms", "0004A30B00215E8C");
+INSERT INTO sensors VALUES (0, "Pression", "hPA", "0004A30B0024D376");
+INSERT INTO sensors VALUES (0, "Température", "Celsius", "0004A30B0024D376");
+INSERT INTO sensors VALUES (0, "Humidité", "rh", "0004A30B0024D376");
+INSERT INTO sensors VALUES (0, "Résistance", "Ohms", "0004A30B0024D376");
 
-INSERT INTO sensors VALUES (0, "TCOV", "ppb", "0004A30B0024A34D");
-INSERT INTO sensors VALUES (0, "CO2", "ppm", "0004A30B0024A34D");
-
-insert into sensorValues (id, date, payload, fk_sensor_id) values (1, '2020-01-23 16:26:49', 860, '#129', 9);
-insert into sensorValues (id, date, payload, fk_sensor_id) values (2, '2019-09-21 03:55:30', 831, '#423', 7);
-insert into sensorValues (id, date, payload, fk_sensor_id) values (3, '2019-06-03 15:27:41', 260, '#791', 2);
-insert into sensorValues (id, date, payload, fk_sensor_id) values (4, '2019-12-03 07:07:17', 422, '#65e', 9);
-insert into sensorValues (id, date, payload, fk_sensor_id) values (5, '2019-07-12 16:05:56', -233, '#2be', 6);
-insert into sensorValues (id, date, payload, fk_sensor_id) values (6, '2019-11-05 22:26:49', -756, '#97c', 6);
-insert into sensorValues (id, date, payload, fk_sensor_id) values (7, '2020-01-25 20:21:47', 568, '#488', 1);
-insert into sensorValues (id, date, payload, fk_sensor_id) values (8, '2019-07-22 23:24:25', 35, '#dd0', 9);
+INSERT INTO sensors VALUES (0, "TCOV", "ppb", "0004A30B00215E8C");
+INSERT INTO sensors VALUES (0, "CO2", "ppm", "0004A30B00215E8C");
